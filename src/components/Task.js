d0c769495d0cd677bc857d5aba981b6ed7dce3b1
Twice <njabulo.magagula@ribccs.com>
2022-09-18 10:17:41 +0200

    + 'x' icons on Task Component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,9 @@
+import { FaTimes } from 'react-icons/fa'
 
 const Task = ({id, text, day}) => {
     return (
         <div className="task">
-            <h3 key={id}>{text}</h3>
+            <h3 key={id}>{text} <FaTimes style={{color: 'red' , cursor: 'pointer'}} /> </h3>
             <p>{ day }</p>
         </div>
     )
