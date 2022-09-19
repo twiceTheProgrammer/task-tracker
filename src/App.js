fa7a30fe862cc620301162df8e6af879a2729636
Twice <njabulo.magagula@ribccs.com>
2022-09-19 17:51:04 +0200

    + onToggle reminder func

--- src/App.js
+++ src/App.js
@@ -6,26 +6,33 @@ import { useState } from "react"
 function App() {
     const [ tasks, setTasks] = useState(
       [
-          { id:1, text: "Doctors Appointment", day: "Feb 5th at 2:30pm", reminder: true },
-          { id:2, text: "Help kids with home work", day: "Feb 5th at 2:30pm", reminder: true },
-          { id:3, text: "Walk the dog", day: "Feb 5th at 2:30pm", reminder: true },
-          { id:4, text: "Watch movies", day: "Feb 5th at 2:30pm", reminder: true },
-          { id:5, text: "Driving lessons", day: "Feb 5th at 2pm", reminder: true },
-          { id:6, text: "studying", day: "Feb 5th at 22:30pm", reminder: true },
-          { id:7, text: "Cleaning", day: "Feb 5th at 12:30pm", reminder: true },
-          { id:8, text: "Group session with friends", day: "Feb 5th at 1:30pm", reminder: true }
+          { id:1, text: "Doctors Appointment", day: "Feb 5th at 2:30pm", reminder: false },
+          { id:2, text: "Help kids with home work", day: "Feb 5th at 2:30pm", reminder: false },
+          { id:3, text: "Walk the dog", day: "Feb 5th at 2:30pm", reminder: false },
+          { id:4, text: "Watch movies", day: "Feb 5th at 2:30pm", reminder: false },
+          { id:5, text: "Driving lessons", day: "Feb 5th at 2pm", reminder: false },
+          { id:6, text: "studying", day: "Feb 5th at 22:30pm", reminder: false },
+          { id:7, text: "Cleaning", day: "Feb 5th at 12:30pm", reminder: false },
+          { id:8, text: "Group session with friends", day: "Feb 5th at 1:30pm", reminder: false }
       ]
   )
 
+  // delete task
   const deleteTask = (id) => 
   {
       setTasks(tasks.filter( (task) => task.id !== id))
   }
 
+  // toggle task
+  const toggleReminder = (id) => 
+  {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ))
+  }
+
   return (
     <div className="container">
       <Header title='Task Tracker'/>
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder}/> : ("No task(s) to show!")}  
     </div>
   );
 }
